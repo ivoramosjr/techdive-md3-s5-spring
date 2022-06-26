5689cccff30130a6d48d2e824a14ddd85fab3a55
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-06-26 13:01:30 -0300

    Criação e configuração inicial do projeto

---
+++ src/main/java/br/senai/jmstechdivespring/JmsTechdiveSpringApplication.java
@@ -0,0 +1,13 @@
+package br.senai.jmstechdivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsTechdiveSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsTechdiveSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/senai/jmstechdivespring/JmsTechdiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.jmstechdivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsTechdiveSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
